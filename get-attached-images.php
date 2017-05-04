5ed16b3732441a9a7da73807002edd2d14ad29d6
Patrick Jackson <pj@solidhex.com>
2017-05-03 23:30:42 -0700

    setup and commit v2.0

---
+++ get-attached-images.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * Get Attached Images Plugin
+ * Plugin Name: Get Attached Images
+ * Author: solidhex
+ * Author URI: https://github.com/solidhex/
+ * Version 2.0
+ */
+
+function get_attached_images( $args = null )
+{
+	
+	/**
+	 * Define the array of defaults
+	 */
+	
+	$defaults = array(
+		'pageID' => false,
+		'size' => 'thumbnail',
+		'single' => false,
+		'prepend' => '<figure>',
+		'append' => '</figure>',
+		'orderby' => 'date',
+		'order' => 'DESC',
+		'echo' => true
+	);
+	
+	$args = wp_parse_args( $args, $defaults );
+	
+	extract( $args, EXTR_SKIP );
+	
+	$output = "";
+
+	// first check if we've passed in a specific page ID
+	if ($pageID != FALSE) {
+		$id = $pageID;
+	} else {
+		global $post;
+		$id = $post->ID;
+	}
+	
+	// now, retrieve all the images
+	$images = get_children(array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby ));
+
+	// set the counter to prevent undefined variable warning
+	$i = 0;
+	
+	if ($images) {
+		if ($echo) {
+			foreach ($images as $image) {
+				$i++;
+
+				if ($image->post_excerpt) {
+					$caption = '<figcaption>' . $image->post_excerpt . '</figcaption>'; 
+				} else {
+					$caption = '';
+				}
+			
+				$output .= $prepend . wp_get_attachment_image($image->ID, $size) . $caption . $append;
+			
+				if ($single == TRUE && $i == 1)  break;
+			}
+
+			return $output;
+		} else {
+			return $images;
+		}
+	} else {
+		return false;
+	}
+}
+
+
+?>
